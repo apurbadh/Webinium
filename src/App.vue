db1a0d170ef22663c4b535307fe28e6db5803f2c
ashshelby <nepalidude3@gmail.com>
2021-10-20 20:35:24 +0545

    added portions in the index page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
   import Navbar from "./components/Navbar.vue"
+  import Upper from './components/Upper/Upper.vue'
+  import First from './components/MIddle/First/First.vue'
 </script>
 
 <template>
   <Navbar/>
+  <Upper/>
+  <First/> 
   <router-view></router-view>
 </template>
 
---
+++ src/components/MIddle/First/First.vue
@@ -0,0 +1,21 @@
+<script lang="ts" >
+export default {
+    name:"First"
+}
+</script>
+<template>
+<section class='first-section-container'>
+
+    <div class="first-image-container">
+            <img class="team-image" src="teamimage.png" alt="Team">
+    </div>
+
+    <div class="first-information-container">
+            <h2 class="first-text">We’ll build you custom websites and manage it too!</h2>
+    </div>
+</section>
+</template>
+
+<style scoped>
+@import './style.css'
+</style>
