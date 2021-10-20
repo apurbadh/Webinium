19995ac61f4cfc903f91f3e938a5d591825ab083
apurbadh <erikduke182@gmail.com>
2021-10-20 13:34:06 +0545

    Navbar created

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-
+  import Navbar from "./components/Navbar.vue"
 </script>
 
 <template>
+  <Navbar/>
   <h1>Webinium</h1>  
 </template>
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export default {
+		name : "Navbar"
+	}
+</script>
+<template>
+	<h2>Navbar</h2>
+</template>
